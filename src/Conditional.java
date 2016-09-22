bbbb12a6e0d270ad857a9016973e55ad4b391daf
“Archit <190902@hkis.edu.hk>
2016-09-22 14:55:25 +0800

    Added infinite loop that is crashing my computer
    :(

---
+++ src/Conditional.java
@@ -0,0 +1,24 @@
+class Conditional{
+    public static void main(String[] args)
+    {
+        System.out.print(2+", ");
+        for (int i = 1; i < 1000; i++)
+        {
+            if (i%2 == 1)
+            {
+                boolean isPrime = true;
+                for (int k = i-1 ; k>= 2 ; k = k - 1)
+                {
+                    if (i % k == 0)
+                    {
+                        isPrime = false;
+                    }
+                }
+                if (isPrime == true)
+                {
+                    System.out.println(i);
+                }
+            }
+        }
+    }
+}
---
+++ src/Loop.java
@@ -0,0 +1,48 @@
+import java.util.*;
+public class Loop {
+public static void main(String[] args)
+{	
+	Scanner input = new Scanner(System.in);
+    
+    System.out.println("Printing Odd numbers between 1 and " + 1000);
+   int limit = 1000;
+  
+    for(int i=1; i <= 1000; i++){
+           
+          
+            if( i % 2 != 0){
+                    System.out.print(i + ", ");}
+    }
+    System.out.println( );
+    System.out.println("Printing even numbers between 1 and " + 1000);         
+    for(int i=1; i <= 1000; i++){           
+    	if( i % 2 == 0){
+    		System.out.print(i + ", ");
+}
+    	 for (int j = 1; j <= 1000; i++)
+	        {
+	            //if (i%2 != 0)
+	            if (j%2 == 1) //odd number
+	            {
+	                boolean isPrime = true;
+	                for (int k = j-1 ; k>= 2 ; k = k - 1)
+	                {
+	                    if (i % k == 0)
+	                    {
+	                        isPrime = false;
+	                        //i is NOT a prime number
+	                    }
+	                }
+	                if (isPrime == true)
+	                {
+	                    //is a prime number!
+	                    System.out.println(i +", ");
+	                }
+	            }
+	        }
+
+    	
+
+}
+}
+}

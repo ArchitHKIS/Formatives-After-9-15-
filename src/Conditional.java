281a8efba57235438c80960fdb94fe05f8fcf2be
“Archit <190902@hkis.edu.hk>
2016-09-24 12:50:22 +0800

    Finally Did the prime number stuff
    No more halp required!

--- src/Conditional.java
+++ src/Conditional.java
@@ -1,22 +1,22 @@
 class Conditional{
     public static void main(String[] args)
     {
-        System.out.print(2);
-        for (int i = 3; i < 1000; i++)
+        System.out.print(2+", ");
+        for (int j = 3; j < 1000; j++)
         {
-            if (i%2 == 1)
+            if (j%2 == 1)
             {
                 boolean isPrime = true;
-                for (int k = i-1 ; k>= 2 ; k = k - 1)
+                for (int k = j-1 ; k>= 2 ; k = k - 1)
                 {
-                    if (i % k == 0)
+                    if (j % k == 0)
                     {
                         isPrime = false;
                     }
                 }
                 if (isPrime == true)
                 {
-                    System.out.println(i);
+                    System.out.print(j + ", ");
                 }
             }
         }
--- src/Loop.java
+++ src/Loop.java
@@ -5,45 +5,39 @@ public static void main(String[] args)
 	Scanner input = new Scanner(System.in);
     
     System.out.println("Printing Odd numbers between 1 and " + 1000);
-   int limit = 1000;
-  
     for(int i=1; i <= 1000; i++){
-           
-          
-            if( i % 2 != 0){
-                    System.out.print(i + ", ");}
+    	if( i % 2 != 0){
+    		System.out.print(i + ", ");}
     }
     System.out.println( );
     System.out.println("Printing even numbers between 1 and " + 1000);         
     for(int i=1; i <= 1000; i++){           
     	if( i % 2 == 0){
-    		System.out.print(i + ", ");}
+    		System.out.print(i + ", ");
+    		}
     	}
-    	// loop going forever
-    int i = 0;
-    	 for (int j = 1; j <= 1000; i++) {
-	            //if (i%2 != 0)
-	            if (j%2 == 1) //odd number
-	            {
-	            	boolean isPrime = true;
-	                for (int k = j-1 ; k>= 2 ; k = k - 1)
-	                {
-	                    if (i % k == 0)
-	                    {
-	                        isPrime = false;
-	                        //i is NOT a prime number
-	                    }
-	                }
-	                if (isPrime == true)
-	                {
-	                    //is a prime number!
-	                    System.out.println(i +", ");
-	                }
-	            }
-	        }
-
-    	
-
+    System.out.println(" ");
+    System.out.println("All prime numbers from 0-1000");
+    for (int j = 3; j < 1000; j++)
+    {
+        if (j%2 == 1)
+        {
+            boolean isPrime = true;
+            for (int k = j-1 ; k>= 2 ; k = k - 1)
+            {
+                if (j % k == 0)
+                {
+                    isPrime = false;
+                }
+            }
+            if (isPrime == true)
+            {
+                System.out.print(j + ", ");
+            }
+        }
+    }
 
 }
 }
+
+		

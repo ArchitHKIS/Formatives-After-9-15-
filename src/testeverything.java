7c0b46357c2f9a67a08528808e6e44dba2cfad93
“Archit <190902@hkis.edu.hk>
2016-09-27 22:34:17 +0800

    wot
    where is the file that i wanted to upload

---
+++ src/testeverything.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class testeverything {
+	public static void main(String[] args){
+		Scanner input = new Scanner(System.in);
+			System.out.println("What is your name:");
+		String name = input.nextLine();
+		String nameL = name.toLowerCase();
+		int stringlength = nameL.length();
+		String reverseName = "";
+		for (int i = stringlength-1; i >= 0; i--){
+		   reverseName += nameL.substring(i, i+1);
+			}
+		System.out.println(reverseName);
+		}
+	}

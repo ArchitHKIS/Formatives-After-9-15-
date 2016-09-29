e714be0bbabed8f69c59657064f079ab7d4d995a
“Archit <190902@hkis.edu.hk>
2016-09-29 21:41:46 +0800

    Added test files for new methods
    Test file

--- src/testeverything.java
+++ src/testeverything.java
@@ -1,15 +1,18 @@
 import java.util.*;
+
 public class testeverything {
-	public static void main(String[] args){
+	public static void main(String[] args) {
+		int k, b, f;
 		Scanner input = new Scanner(System.in);
-			System.out.println("What is your name:");
+		System.out.println("What is your name:");
 		String name = input.nextLine();
 		String nameL = name.toLowerCase();
 		int stringlength = nameL.length();
 		String reverseName = "";
-		for (int i = stringlength-1; i >= 0; i--){
-		   reverseName += nameL.substring(i, i+1);
-			}
-		System.out.println(reverseName);
+		for (int i = stringlength - 1; i >= 0; i--) {
+			reverseName += nameL.substring(i, i + 1);
 		}
+		System.out.println(reverseName);
+		System.out.println(b);
 	}
+}

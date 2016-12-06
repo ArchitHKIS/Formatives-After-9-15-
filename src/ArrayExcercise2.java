0cbfe80d66f7d5be27358a2a2f7c7d6fbba963cb
“Archit <190902@hkis.edu.hk>
2016-12-06 10:24:35 +0800

    Removed useless comments
    =)

--- src/ArrayExcercise2.java
+++ src/ArrayExcercise2.java
@@ -8,12 +8,6 @@ public class ArrayExcercise2 {
 				bobo[i][b] = ThreadLocalRandom.current().nextInt(0, 10);
 			}
 		}
-		/*for (int i = 0; i < bobo.length; i++) {
-			for (int b = 0; b < bobo[0].length; b++) {
-				System.out.print(bobo[i][b] + " ");
-			}
-			System.out.println("");
-		}*/
 		ArrayExcercise2.printArray(bobo);
 	}
 
